fcdf33b8eb5bcbd30e56ee563ddd06a079cd1a7b
Dr Asis Hallab <a.hallab@fz-juelich.de>
2021-04-23 13:38:05 +0200

    Added functions to parse reference data
    - Can read HMMER3 tblout fixed width tables.
    - Abandoned lazy loading. Was too slow.
    - Added function to generate sets of informative
      words from human readable protein descriptions (HRD).

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
-# Function ensured data-sets to be loaded, whenever this package is loaded.
-.onLoad <- function( libname = find.package( "prot.scriber" ), pkgname = "prot.scriber" ) {
-    data( "regexLists", package = "prot.scriber" )
-    data( "p_coccineus_seq_sin_search", package = "prot.scriber" )
+#' Function ensured data-sets to be loaded, whenever this package is loaded.
+.onLoad <- function(libname = find.package("prot.scriber"), pkgname = "prot.scriber") {
+    data("regexLists", package = "prot.scriber")
+    #' data( 'p_coccineus_seq_sin_search', package = 'prot.scriber' )
+    message("To load P. coccineus sequence similarity search results use the following command:\n", 
+        "data( \"p_coccineus_seq_sin_search\", package = \"prot.scriber\" )")
 }

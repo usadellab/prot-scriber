618288a420da1f138d489793c3587eb1b35ae865
Dr Asis Hallab <a.hallab@fz-juelich.de>
2021-04-29 14:50:03 +0200

    Added executable script to load references for P.coccineus

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 #' Function ensured data-sets to be loaded, whenever this package is loaded.
 .onLoad <- function(libname = find.package("prot.scriber"), pkgname = "prot.scriber") {
     data("regexLists", package = "prot.scriber")
-    #' data( 'p_coccineus_seq_sin_search', package = 'prot.scriber' )
-    message("To load P. coccineus sequence similarity search results use the following command:\n", 
-        "data( \"p_coccineus_seq_sin_search\", package = \"prot.scriber\" )")
+    data("p_coccineus_reference_words", package = "prot.scriber")
+    message("To load data from prot.scriber that is not loaded automatically, please use e.g.:\n", 
+        "data( \"p_coccineus_seq_sim_search\", package = \"prot.scriber\" )")
+    message("Available datasets in package 'prot.scriber' are:\n", paste(data(package = "prot.scriber")$results[, 
+        "Item"], collapse = ", "))
 }
--- exec/loadPcoccineus_PfamA_Mercator4_results.R
+++ exec/loadPcoccineus_PfamA_Mercator4_results.R
@@ -2,36 +2,15 @@
 
 require(prot.scriber)
 
-option_list <- list(
-  make_option(
-    c("-m", "--mercator-table"),
-    type = "character",
-    default = NULL,
-    help = "Valid file path to the result of running Mercator4",
-    metavar = "character"
-  ),
-  make_option(
-    c("-p", "--hmmer3-vs-pfamA-tblout"),
-    type = "character",
-    default = NULL,
-    help = "Valid file path to `--tblout` result of running HMMER3 PfamA",
-    metavar = "character"
-  ),
-  make_option(
-    c("-d", "--data-dir"),
-    type = "character",
-    default = NULL,
-    help = "The directory into which to write the result binary 'p_coccineus_seq_sin_search.RData'",
-    metavar = "character"
-  ),
-  make_option(
-    c("-n", "--n-cores"),
-    type = "numeric",
-    default = detectCores(),
-    help = "The number of cores to use in parallel processes.",
-    metavar = "numeric"
-  )
-)
+option_list <- list(make_option(c("-m", "--mercator-table"), 
+    type = "character", default = NULL, help = "Valid file path to the result of running Mercator4", 
+    metavar = "character"), make_option(c("-p", "--hmmer3-vs-pfamA-tblout"), 
+    type = "character", default = NULL, help = "Valid file path to `--tblout` result of running HMMER3 PfamA", 
+    metavar = "character"), make_option(c("-d", "--data-dir"), 
+    type = "character", default = NULL, help = "The directory into which to write the result binary 'p_coccineus_seq_sin_search.RData'", 
+    metavar = "character"), make_option(c("-n", "--n-cores"), 
+    type = "numeric", default = detectCores(), help = "The number of cores to use in parallel processes.", 
+    metavar = "numeric"))
 
 opt_parser <- OptionParser(option_list = option_list)
 args <- parse_args(opt_parser)
@@ -49,5 +28,21 @@ pc.pfamA <- parseHmmer3Tblout(args$`hmmer3-vs-pfamA-tblout`)
 save(pc.mercator, pc.pfamA, file = file.path(args$`data-dir`, 
     "p_coccineus_pfamA_Mercator4.RData"))
 
-#' DONE
+#' Generate reference word list from P.coccineus PfamA:
+pc.ref.pfamA <- referenceWordListFromPfamAAnnos(pc.pfamA)
+
+#' Generate reference word list from P.coccineus Mercator4:
+pc.ref.mercator <- referenceWordListFromMercator4Annos(pc.mercator)
+
+#' Save the two reference sets for future use:
+save(pc.ref.mercator, pc.ref.pfamA, file = file.path(args$`data-dir`, 
+    "p_coccineus_mercator_pfamA_references.RData"))
+
+#' Merge (union) these reference lists:
+pc.ref <- mergeMercatorAndPfamAReferences(pc.ref.mercator, pc.ref.pfamA)
+
+#' Save merged references:
+save(pc.ref, file = file.path(args$`data-dir`, "p_coccineus_reference_words.RData"))
+
+#' DONE:
 message("DONE")

fccf7612f41603c5f5f1d3f4f5c6fa4844963418
Atemia <j.atemia@fz-juelich.de>
2021-12-02 20:27:16 +0100

    wip: implementing get hdr phrases function

--- src/generate_HRD_associated_funcs.rs
+++ src/generate_HRD_associated_funcs.rs
@@ -1,22 +1,20 @@
-// extern crate linked_hash_set;
-// use linked_hash_set::LinkedHashSet;
 use regex::Regex;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
-
-// Given filtered candidate descriptions it splits each word and returns a vector. 
+/// Given filtered candidate descriptions it splits each word and returns a vector. 
 pub fn split_candidates_descripts(candidate_words:&str)-> Vec<&str> {    
     let mut word_vec : Vec<&str> = candidate_words.split(|c : char| c.is_whitespace()).collect();
     word_vec.push(candidate_words); // add the original candidate word
     word_vec
 }
 
-// creates a map/dictionary containing individual candidate descriptions(keys) and if its informative (value : bool).
-// The matches are converted by the if condition to match the logic. bool (True) is assigned to an informative word.
+/// creates a map/dictionary containing individual candidate descriptions(keys) and if its informative (value : bool).
+/// The matches are converted by the if condition to match the logic. bool (True) is assigned to an informative word.
 pub fn word_classification_map(candidate_word_vec:Vec<&str>,re:Vec<Regex>) -> HashMap<&str,bool> {
     let mut word_classif = HashMap::new();
     for w in candidate_word_vec{
-        // let mut tag = false;
         if matches_uninformative_list(&w, &re) == true {
             let tag = false;
             word_classif.insert(w,  tag);
@@ -27,7 +25,7 @@ pub fn word_classification_map(candidate_word_vec:Vec<&str>,re:Vec<Regex>) -> Ha
     };
     word_classif
 }
-// When parsed a set of regex strings in a vector it matches and returns true if match.
+/// When parsed a set of regex strings in a vector it matches and returns true if match.
 pub fn matches_uninformative_list(word: &str, regex: &Vec<Regex>) -> bool {
     regex.iter().any(|x| x.is_match(&word.to_string()))
 }
@@ -58,7 +56,47 @@ pub fn phrasesFromCandidateDescription( word_vec : Vec<String>, word_classif : H
     Phrase_info   
 }
 
-// phrasesFromCandidateDescription(word_vec , world_classif);
+
+/// Powersets any slice
+pub fn powerset<T: Clone>(slice: &[T]) -> Vec<Vec<T>> {
+    let mut v: Vec<Vec<T>> = Vec::new();
+
+    for mask in 0..(1 << slice.len()) {
+        let mut ss: Vec<T> = vec![];
+        let mut bitset = mask;
+        while bitset > 0 {
+            // isolate the rightmost bit to select one item
+            let rightmost: u64 = bitset & !(bitset - 1);
+            // turn the isolated bit into an array index
+            let idx = rightmost.trailing_zeros();
+            let item = (*slice.get(idx as usize).unwrap()).clone();
+            ss.push(item);
+            // zero the trailing bit
+            bitset &= bitset - 1;
+        }
+        v.push(ss);
+    }
+    v
+}
+
+
+
+/// Creates a hashset from a Vector. Not genetic. 
+/// But can be tweaked to take other types carried by a vector.
+fn hashset(data: &Vec<String>) -> HashSet<String> {
+    HashSet::from_iter(data.iter().cloned())
+}
+
+fn get_possiable_hrd_phrases(candidate_vector_descriptions){
+    let  p = powerset(candidate_vector_descriptions);
+
+    for i in p {
+        let vector_String: Vec<String> = i.iter().map(|s| s.to_string()).collect();
+        println!("{:?}\n{:?}", &i, hashset(&vector_String));
+}  
+}
+
+
 
 
 #[cfg(test)]

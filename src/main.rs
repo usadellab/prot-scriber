a13132dac22b786d634792da7a516dd332c73e61
wunderbarr <wunderbardan@gmail.com>
2022-01-04 16:01:36 +0100

    format: format code & update messages

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ mod annotation_process;
 mod default;
 mod hit;
 mod model_funcs;
+mod output_writer;
 mod playground;
 mod query;
 mod seq_family;
 mod seq_family_reader;
 mod seq_sim_table_reader;
-mod output_writer;
 
 fn main() {
     let matches = App::new("Prot-Scriber")
@@ -82,9 +82,15 @@ fn main() {
 
     // Save output:
     if let Some(o) = matches.value_of("output") {
-        match output_writer::write_output_table(o.to_string(), annotation_process.human_readable_descriptions){
-            Ok(()) => println!("successfully write to the file!"),
-            Err(e) => eprintln!("encounter an error! :( \n  {}", e),
+        match output_writer::write_output_table(
+            o.to_string(),
+            annotation_process.human_readable_descriptions,
+        ) {
+            Ok(()) => println!("output written to file {}!", o),
+            Err(e) => eprintln!(
+                "We are sorry, an error occurred when attempting to write output to file {} \n{}",
+                o, e
+            ),
         };
     }
 }
--- src/output_writer.rs
+++ src/output_writer.rs
@@ -7,8 +7,11 @@ use std::fs::write;
 ///
 /// * `file_path: String` - The file path for saving output.
 /// * `human_readable_descriptions: HashMap<String, String>` - The generated human readable descriptions.
-pub fn write_output_table( file_path: String, human_readable_descriptions: HashMap<String, String> ) -> std::io::Result<()>{
-    if human_readable_descriptions.keys().len()>0 {
+pub fn write_output_table(
+    file_path: String,
+    human_readable_descriptions: HashMap<String, String>,
+) -> std::io::Result<()> {
+    if human_readable_descriptions.keys().len() > 0 {
         let mut output = String::from("Annotee-Identifier\tHuman-Readable-Description");
         // stream write line after line
         // iterate over entries in argument human_readable_descriptions
@@ -16,8 +19,8 @@ pub fn write_output_table( file_path: String, human_readable_descriptions: HashM
             output.push_str(&(format!("\n{}\t{}", annotee_name, annotation)));
         }
         return write(file_path, output);
-    } else{
-        Ok(())    
+    } else {
+        Ok(())
     }
 }
 
@@ -27,9 +30,22 @@ mod tests {
     use std::collections::HashMap;
     #[test]
     fn writer_test() {
-        let mut human_readable_descriptions:HashMap<String, String> = HashMap::new();
-        human_readable_descriptions.insert("Seq-Family-1".to_string(), "alien devouring protein".to_string());
-        human_readable_descriptions.insert("Protein-123".to_string(), "human devouring protein".to_string());
-        assert_eq!(write_output_table("./target/result.txt".to_string(), human_readable_descriptions).is_ok(), true);
+        let mut human_readable_descriptions: HashMap<String, String> = HashMap::new();
+        human_readable_descriptions.insert(
+            "Seq-Family-1".to_string(),
+            "alien devouring protein".to_string(),
+        );
+        human_readable_descriptions.insert(
+            "Protein-123".to_string(),
+            "human devouring protein".to_string(),
+        );
+        assert_eq!(
+            write_output_table(
+                "./target/result.txt".to_string(),
+                human_readable_descriptions
+            )
+            .is_ok(),
+            true
+        );
     }
 }
